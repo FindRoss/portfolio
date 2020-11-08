70140cfbdc4b3fb10f03d72b7987f296dbb69837
Ross Findlay <r.findlay.05@aberdeen.ac.uk>
2020-11-08 15:06:47 +0100

    updated projects

--- src/components/Projects/data.js
+++ src/components/Projects/data.js
@@ -1,33 +1,15 @@
-import footy from "../../images/footy-fixture-project-compressor.png";
-import higher from "../../images/higher-lower-project-compressor.png";
 import species from "../../images/star-wars-species-project.png";
 import todo from "../../images/react-todo-list-project.png"
+// import higher from "../../images/higher-lower-project-compressor.png";
+// import footy from "../../images/footy-fixture-project-compressor.png";
 
 export const projectData = [
-  {
-    image: higher,
-    imgStart: '',
-    title: "Higher or lower",
-    description:
-      "A simple higher or lower dice game. The player gets three lives, and must better the computer.",
-    used: ["React"],
-    url: "!#"
-  },
-  {
-    image: footy,
-    imgStart: 'start',
-    title: "Football fixtures",
-    description:
-      "Live football fixtures for the top leagues and competitions in the world with the api.",
-    used: ["React", "Redux", "Evergreen UI"],
-    url: "!#"
-  },
   {
     image: species,
     imgStart: '',
     title: "Star Wars Species",
     description:
-      "A catalogue of Star Wars species fetched from an API. Mon Calarami are the best.",
+      "A catalogue of the types of alients found in the Star Wars world. Fetched using the Star Wars API, built with React, React Hooks, React Router, and designed with help of Grommet. Check out the Mon Calarami.",
     used: ["React", "React Router", "Hooks", "Grommet"],
     url: "https://star-wars-species.herokuapp.com"
   },
@@ -35,8 +17,26 @@ export const projectData = [
     image: todo,
     imgStart: 'start',
     title: "Todo list",
-    description: "FOOOOBAR!! Change the APP ICON and APP TITLE",
+    description: "A simple todo list SPA built using React and styled with Material UI.",
     used: ["React"],
     url: "https://rf-react-list.herokuapp.com"
   }
+  // {
+  //   image: higher,
+  //   imgStart: '',
+  //   title: "Higher or lower",
+  //   description:
+  //     "A simple higher or lower dice game. The player gets three lives, and must better the computer.",
+  //   used: ["React"],
+  //   url: "!#"
+  // },
+  // {
+  //   image: footy,
+  //   imgStart: 'start',
+  //   title: "Football fixtures",
+  //   description:
+  //     "Live football fixtures for the top leagues and competitions in the world with the api.",
+  //   used: ["React", "Redux", "Evergreen UI"],
+  //   url: "!#"
+  // },
 ];
